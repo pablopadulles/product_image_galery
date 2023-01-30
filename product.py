4d88bd2c0f094d058da870eb624945d16670c47e
Pablo <pablopadulles@gmail.com>
2023-01-29 22:09:22 -0300

    Agrego boton para crear thumbnail desde la imagen

--- product.py
+++ product.py
@@ -1,10 +1,13 @@
+from PIL import Image
+import io
+
 from trytond.model import (
     DeactivableMixin, Exclude, Model, ModelSQL, ModelView, UnionMixin, fields,
     sequence_ordered)
 from trytond.modules.company.model import (
     CompanyMultiValueMixin, CompanyValueMixin)
 from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval, Get, If
+from trytond.pyson import Eval, Get, If, Bool
 from trytond.tools import lstrip_wildcard
 from trytond.tools.multivalue import migrate_property
 from trytond.transaction import Transaction
@@ -25,6 +28,7 @@ class ProductImageGalery(sequence_ordered(), ModelSQL, ModelView):
     __name__ = 'product.image.galery'
 
     name = fields.Char('Name', required=True)
+    thumbnail_name = fields.Char('Thumbnail Name', states={'required': If(Bool(Eval('thumbnail', False)), True, False)})
     template = fields.Many2One(
         'product.template', "Product Template",
         required=True, ondelete='CASCADE', select=True,
@@ -36,7 +40,7 @@ class ProductImageGalery(sequence_ordered(), ModelSQL, ModelView):
         "inherited by the variant.")
     data = fields.Binary('Data', filename='name',
         file_id=file_id, store_prefix=store_prefix)
-    thumbnail = fields.Binary('Data Thumbnail', filename='name',
+    thumbnail = fields.Binary('Data Thumbnail', filename='thumbnail_name',
         file_id=thumbnail_id, store_prefix=store_prefix)
     file_id = fields.Char('File ID', readonly=True)
     thumbnail_id = fields.Char('Thumbnail ID', readonly=True)
@@ -44,6 +48,15 @@ class ProductImageGalery(sequence_ordered(), ModelSQL, ModelView):
     path_thumbnail = fields.Function(fields.Char('Path thumbnail'), 'get_path')
     images = fields.Function(fields.Char('Path Images'), 'get_images_path')
 
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls._buttons.update({
+            'generate_thumbnail': {
+                'invisible': False,
+                'icon': 'tryton-ok',
+            },
+        })
     def get_path(self, name):
         path = ''
         if name == 'path_data':
@@ -59,6 +72,19 @@ class ProductImageGalery(sequence_ordered(), ModelSQL, ModelView):
             dic['path_thumbnail'] = '/' + self.thumbnail_id[:2] + '/' + self.thumbnail_id[2:4] + '/' + self.thumbnail_id
     
         return dic
+
+    @classmethod
+    @ModelView.button
+    def generate_thumbnail(cls, pigs):
+        data = io.BytesIO()
+        for pig in pigs:
+            MAX_SIZE = (300, 300)
+            image = Image.open(io.BytesIO(pig.data))
+            image.thumbnail(MAX_SIZE)
+            pig.thumbnail_name = 'thumbnail-' + pig.name
+            image.save(data, format='jpeg', optimize=True)
+            pig.thumbnail = data.getvalue()
+            pig.save()
 
 
 class Template(metaclass=PoolMeta):
